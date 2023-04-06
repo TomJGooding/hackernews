355b53c223562d7be89f0f774bea0a043eb843b6
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-04-06 23:29:13 +0100

    feat(api): get top stories

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class HackerNewsApi:
+    BASE_URL: str = "https://hacker-news.firebaseio.com/v0/"
+
+    def get_top_stories(self) -> dict:
+        resource: str = "topstories.json"
+        return self._request(resource)
+
+    def _request(self, resource: str) -> dict:
+        # TODO: handle this properly!
+        data = dict()
+        resp = requests.get(self.BASE_URL, resource)
+        data = resp.json()
+        return data
