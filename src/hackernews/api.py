cd7d58730f2ef6af30e745e7a5d0a74b022399e9
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-04-07 20:53:41 +0100

    feat: change to aiohttp for faster load

--- src/hackernews/api.py
+++ src/hackernews/api.py
@@ -1,4 +1,7 @@
-import requests
+import asyncio
+from typing import Any
+
+import aiohttp
 
 from hackernews.models import Item
 
@@ -6,19 +9,39 @@ from hackernews.models import Item
 class HackerNewsApi:
     BASE_URL: str = "https://hacker-news.firebaseio.com/v0/"
 
-    def get_top_stories(self) -> dict:
-        resource: str = "topstories.json"
-        return self._request(resource)
+    def __init__(self) -> None:
+        self._session = aiohttp.ClientSession()
 
-    def get_item(self, id: int) -> Item:
+    async def __aenter__(self) -> aiohttp.ClientSession:
+        return self._session
+
+    async def __aexit__(self, *errs: Any) -> None:
+        await self._session.close()
+
+    async def get_top_stories_ids(self) -> dict:
+        resource: str = "topstories.json"
+        return await self._get(resource)
+
+    async def get_item(self, id: int) -> Item:
         resource: str = f"item/{id}.json"
-        data: dict = self._request(resource)
+        data: dict = await self._get(resource)
         item: Item = Item.from_dict(data)
         return item
 
-    def _request(self, resource: str) -> dict:
-        # TODO: handle this properly!
+    async def get_multiple_items(self, item_ids: list[int]) -> list[Item]:
+        tasks = []
+        for id in item_ids:
+            tasks.append(asyncio.ensure_future(self.get_item(id)))
+
+        item_list = await asyncio.gather(*tasks)
+        return item_list
+
+    async def _get(self, resource: str) -> dict:
         data = dict()
-        resp = requests.get(f"{self.BASE_URL}{resource}")
-        data = resp.json()
-        return data
+        async with self._session.get(f"{self.BASE_URL}{resource}") as resp:
+            try:
+                data = await resp.json()
+            except aiohttp.ContentTypeError as e:
+                raise e
+
+            return data
