6c39b894622f7e8b3880a308590cd631610f5a5c
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-04-07 13:15:22 +0100

    feat(api): get item by id

--- src/hackernews/main.py
+++ src/hackernews/main.py
@@ -8,6 +8,10 @@ class HackerNewsApi:
         resource: str = "topstories.json"
         return self._request(resource)
 
+    def get_item(self, id: int) -> dict:
+        resource: str = f"item/{id}.json"
+        return self._request(resource)
+
     def _request(self, resource: str) -> dict:
         # TODO: handle this properly!
         data = dict()
